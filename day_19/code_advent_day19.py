0a23fb1a43ac553482557b53aba719c26a7208f5
Kelsey <kelsey@edmodo.com>
2016-01-17 22:36:26 -0800

    more code

--- day_19/code_advent_day19.py
+++ day_19/code_advent_day19.py
@@ -1,7 +1,10 @@
 # code_advent_day19
 # replace one step
 
-# 193
+
+# input_rules = ['H => HO',
+# 'H => OH',
+# 'O => HH']
 
 input_rules = ['Al => ThF',
 'Al => ThRnFAr',
@@ -47,9 +50,10 @@ input_rules = ['Al => ThF',
 'e => NAl',
 'e => OMg']
 
+magic_medicine = "CRnSiRnCaPTiMgYCaPTiRnFArSiThFArCaSiThSiThPBCaCaSiRnSiRnTiTiMgArPBCaPMgYPTiRnFArFArCaSiRnBPMgArPRnCaPTiRnFArCaSiThCaCaFArPBCaCaPTiTiRnFArCaSiRnSiAlYSiThRnFArArCaSiRnBFArCaCaSiRnSiThCaCaCaFYCaPTiBCaSiThCaSiThPMgArSiRnCaPBFYCaCaFArCaCaCaCaSiThCaSiRnPRnFArPBSiThPRnFArSiRnMgArCaFYFArCaSiRnSiAlArTiTiTiTiTiTiTiRnPMgArPTiTiTiBSiRnSiAlArTiTiRnPMgArCaFYBPBPTiRnSiRnMgArSiThCaFArCaSiThFArPRnFArCaSiRnTiBSiThSiRnSiAlYCaFArPRnFArSiThCaFArCaCaSiThCaCaCaSiRnPRnCaFArFYPMgArCaPBCaPBSiRnFYPBCaFArCaSiAl"
+
 input_word = "CRnSiRnCaPTiMgYCaPTiRnFArSiThFArCaSiThSiThPBCaCaSiRnSiRnTiTiMgArPBCaPMgYPTiRnFArFArCaSiRnBPMgArPRnCaPTiRnFArCaSiThCaCaFArPBCaCaPTiTiRnFArCaSiRnSiAlYSiThRnFArArCaSiRnBFArCaCaSiRnSiThCaCaCaFYCaPTiBCaSiThCaSiThPMgArSiRnCaPBFYCaCaFArCaCaCaCaSiThCaSiRnPRnFArPBSiThPRnFArSiRnMgArCaFYFArCaSiRnSiAlArTiTiTiTiTiTiTiRnPMgArPTiTiTiBSiRnSiAlArTiTiRnPMgArCaFYBPBPTiRnSiRnMgArSiThCaFArCaSiThFArPRnFArCaSiRnTiBSiThSiRnSiAlYCaFArPRnFArSiThCaFArCaCaSiThCaCaCaSiRnPRnCaFArFYPMgArCaPBCaPBSiRnFYPBCaFArCaSiAl"
-
-
+#input_word = "HOHOHO"
 
 def rule_by_letter(input_rules):
 	rule_dict = {}
@@ -60,34 +64,31 @@ def rule_by_letter(input_rules):
 			rule_dict[i.split()[0]] = [i.split()[2]]
 	return rule_dict
 
-# def word_letter_count(input_word):
-# 	letter_count = {}
-# 	for i in input_word:
-# 		if i in letter_count.keys():
-# 			letter_count[i] += 1
-# 		else:
-# 			letter_count[i] = 1
-# 	print letter_count
-# 	return letter_count
+
 
 def main(input_rules, input_word):
 	count = 0
 	new_words = []
 	rule_dict = rule_by_letter(input_rules)
-	#letter_count = word_letter_count(input_word)
-	for i in range(0,len(input_word)):
-		if input_word[i] in rule_dict.keys():
-			for j in rule_dict[input_word[i]]:
-				if i > 0:
-					temp = str(input_word[:i]) + j + str(input_word[i+1:])
-					if temp not in new_words:
-						new_words.append(temp)
-				else:
-					temp = j + str(input_word[i+1:])
+	for i in rule_dict.keys():
+		for j in range(0, len(input_word)):
+			if len(rule_dict[i])>1:
+				for k in rule_dict[i]:
+					if input_word[j:j+len(i)] == i:
+						temp = str(input_word[:j]) + k + str(input_word[j+len(i):])
+						if temp not in new_words:
+							new_words.append(temp)
+
+			else:
+				if input_word[j:j+len(i)] == i:
+					temp = str(input_word[:j]) + str(rule_dict[i][0]) + str(input_word[j+len(i):])
 					if temp not in new_words:
 						new_words.append(temp)
 	print len(new_words)
-				
+
+
+
+			
 main(input_rules, input_word)
 
 
--- day_21/code_advent_day21.py
+++ day_21/code_advent_day21.py
@@ -4,8 +4,6 @@
 #you have 8 hit points, 5 damage, and 5 armor, 
 # and that the boss has 12 hit points, 7 damage, and 2 armor:
 
-## MIN COST IS 91
-
 # boss = {"Hit_Points": 12,
 # "Damage": 7,
 # "Armor": 2}
@@ -19,51 +17,67 @@ boss = {"Hit_Points": 109,
 "Armor": 2}
 
 player = {"Hit_Points": 100,
-"Damage": 1,
-"Armor": 1}
+"Damage": 0,
+"Armor": 0}
 
-# so long as I pick d & a such that d-2 >= 8-a  i.e. d+a >= 10
-# so min(cost) of (1)W + (0-1)M + (0-2)R such that d+d+d+a+a+a >=10
+# so long as I pick d & a such that (d-2)*100 >= (8-a)*109
+#       100d - 200 >= 872 - 109a
+# i.e.  100d + 109a >= 1072
 
-#[cost, d+a]
-Weapons = [[8,4],[10,5],[25,6],[40,7],[74,8]]
-Armor = [[13,1],[31,2],[53,3],[75,4],[102,5]]
-Rings = [[25,1],[50,2],[100,3],[20,1],[40,2],[80,3]]
+# so min(cost) of (1)W + (0-1)M + (0-2)R such that 100d + 109a >= 1072
+
+#[cost, d, a]
+Weapons = [[8,4,0],[10,5,0],[25,6,0],[40,7,0],[74,8,0]]
+Armor = [[13,0,1],[31,0,2],[53,0,3],[75,0,4],[102,0,5]]
+Rings = [[25,1,0],[50,2,0],[100,3,0],[20,0,1],[40,0,2],[80,0,3]]
 
 def combos(listA, listB, starter):
 	combo= starter
 	for i in listA:
 		combo.append(i)
 	if listA == listB:
-		for i in range(0,len(listA)-1):
-			for j in range(i+1,len(listB)):
-				ad = listA[i][1] + listB[j][1]
-				cost = listA[i][0] + listB[j][0] 
-				combo.append([cost,ad])
-		print combo
+		for i in range(0,len(listA)):
+			for j in range(0,len(listB)):
+				if listA[i] != listB[j]:
+					d = listA[i][1] + listB[j][1]
+					a = listA[i][2] + listB[j][2]
+					cost = listA[i][0] + listB[j][0] 
+					combo.append([cost,d,a])
 	else:
 		for i in range(0,len(listA)):
 			for j in range(0,len(listB)):
-				ad = listA[i][1] + listB[j][1]
+				d = listA[i][1] + listB[j][1]
+				a = listA[i][2] + listB[j][2]
 				cost = listA[i][0] + listB[j][0] 
-				combo.append([cost,ad])
+				combo.append([cost,d,a])
 	return combo
 
-min_cost = 10000
-all_rings = combos(Rings,Rings,[[0,0]])
-wps_amr = combos(Weapons,Armor,[])
-for i in wps_amr:
-	for j in all_rings:
-		ad = i[1]+j[1]
-		cost = i[0]+j[0]
-		if ad >= 10:
-			if cost < min_cost:
-				min_cost = cost
-print min_cost
+def main_win():
+	options = []
+	min_cost = 10000
+	max_cost = 0
+	all_rings = combos(Rings,Rings,[[0,0,0]])
+	wps_amr = combos(Weapons,Armor,[])
+	for i in wps_amr:
+		for j in all_rings:
+			d = i[1]+j[1]
+			a = i[2]+j[2]
+			cost = i[0]+j[0]
+			options.append([cost,d,a])
+			if d*100 + a*109 >= 1072:
+				if cost < min_cost:
+					min_cost = cost
+			if d*100 + a*109 < 1072:
+				if cost > max_cost:
+					max_cost = cost
+	print "Min cost is: " + str(min_cost)
+	print "Max cost is: " + str(max_cost)
+	return options
 
+main_win()
 
 ## #####################################################
-## This part plays the game, but isn't strictly necessary.
+## This part plays the game, but isn't needed.
 
 # def hit_calc(a, b):
 # 	if int(a["Damage"]) - int(b["Armor"]) > 1:
@@ -73,13 +87,16 @@ print min_cost
 # 	return hit
 
 
-
 # def game(boss, player):
+# 	winner = ""
 # 	boss_hit = hit_calc(boss, player)
 # 	player_hit = hit_calc(player, boss)
 # 	if int(boss["Hit_Points"])/player_hit <= int(player["Hit_Points"])/boss_hit:
-# 		print "player_wins"
+# 		winner = "player"
 # 	else:
-# 		print "boss_wins"
+# 		winner = "boss"
+# 	return winner
 
-# game(boss,player)
+
+## ###################################################
+
